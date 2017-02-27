6ea257e29629c0564e8438cf50f5daa55877c21a
nagracks <nagracks@users.noreply.github.com>
2017-02-27 19:18:16 +0530

    Refactor

--- media_tools.py
+++ media_tools.py
@@ -10,6 +10,7 @@ import os
 import subprocess
 from argparse import ArgumentParser
 
+
 class MediaTools(object):
     """MediaTools
 
@@ -24,22 +25,27 @@ class MediaTools(object):
 
     def __init__(self, source):
         self.source = source
-        # Get file name and file extension
-        self.fn, self.fe = os.path.splitext(source)
+        self.fname, self.fext = os.path.splitext(source)
 
     def convert_to_mp3(self):
         """Convert any media file to mp3
 
         :returns: None
         """
-        in_file = self.source
-        out_file = self.fn + '.mp3'
-        # ffmpeg command, converts input file to mp3
-        # -b:a, audio bit rate of mp3 file set to 128kbps
-        # -vn, disable video recording
-        ffmpeg_cmd = ("ffmpeg -i {in_file} -b:a 128K -vn {out_file}"
-                      "".format(in_file=in_file, out_file=out_file))
-        subprocess.call(ffmpeg_cmd, shell=True)
+        infile = self.source
+        outfile = self.fname + '.mp3'
+        # ffmpeg command to converts input file to mp3
+        # -b:a: audio bit rate of mp3 file set to 128kbps
+        # -vn: disable video recording
+        ffmpeg_cmd = (
+                "ffmpeg -i {infile} -b:a 128K -vn {outfile}".format(
+                    infile=infile, outfile=outfile
+                    )
+                )
+        subprocess.call(
+                ffmpeg_cmd, shell=True, 
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                )
 
     def cut_it(self, start, stop):
         """Cut from mp3 files
@@ -48,60 +54,67 @@ class MediaTools(object):
         :stop: int, stop point
         :returns: None
         """
-        in_file = self.source
-        out_file = self.fn + '_cutted' + '.mp3'
-        ffmpeg_cmd = ("ffmpeg -ss {start} -t {stop} -i {in_f} {out_f}"
-                      "".format(start=start, stop=stop, 
-                                in_f=in_file, out_f=out_file))
-        subprocess.call(ffmpeg_cmd, shell=True)
+        infile = self.source
+        outfile = self.fname + '_cutted' + '.mp3'
+        ffmpeg_cmd = (
+                "ffmpeg -ss {start} -t {stop} -i {infile} {outfile}".format(
+                    start=start, stop=stop, 
+                    infile=infile, outfile=outfile
+                    )
+                )
+        subprocess.call(
+                ffmpeg_cmd, shell=True, 
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                )
 
     def convert_to_mp4(self):
         """Convert any media file to mp4
 
         :returns: None
         """
-        in_file = self.source
-        out_file = self.fn + '.mp4'
-        ffmpeg_cmd = ("ffmpeg -i {in_file} -codec copy {out_file}"
-                      "".format(in_file=in_file, out_file=out_file))
-        subprocess.call(ffmpeg_cmd, shell=True)
-
-
-def parse_args():
-    """Parse commandline args 
-    :returns: args
-    """
-    parser = ArgumentParser(description="Media Tools")
-    parser.add_argument('-v', '--version',
-            action='version',
-            version='%(prog)s version 0.1')
-
-    parser.add_argument('source',
-            action='store',
-            help="source file")
-
-    parser.add_argument('-cut',
-            dest='cut',
-            nargs=2,
-            metavar=('Start', 'Stop'),
-            action='store',
-            help="Cut mp3 files")
-
-    parser.add_argument('-mp3',
-            dest='mp3',
-            action='store_true',
-            help="Convert to mp3")
-
-    parser.add_argument('-mp4',
-            dest='mp4',
-            action='store_true',
-            help="Convert to mp4")
-
-    return parser.parse_args()
+        infile = self.source
+        outfile = self.fname + '.mp4'
+        ffmpeg_cmd = (
+                "ffmpeg -i {infile} -codec copy {outfile}".format(
+                    infile=infile, outfile=outfile
+                    )
+                )
+        subprocess.call(
+                ffmpeg_cmd, shell=True, 
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                )
 
 
 if __name__ == "__main__":
-    args = parse_args()
+    # Parse commandline options with argparse
+    parser = ArgumentParser(description="Media Tools")
+    parser.add_argument(
+            '-v', '--version',
+            action='version',
+            version='%(prog)s version 0.1'
+            )
+    parser.add_argument(
+            'source',
+            action='store',
+            help="source file"
+            )
+    parser.add_argument(
+            '-cut',
+            nargs=2,
+            metavar=('Start', 'Stop'),
+            action='store',
+            help="Cut mp3 files"
+            )
+    parser.add_argument(
+            '-mp3',
+            action='store_true',
+            help="Convert to mp3"
+            )
+    parser.add_argument(
+            '-mp4',
+            action='store_true',
+            help="Convert to mp4")
+    args = parser.parse_args()
 
     media_tools = MediaTools(args.source)
 
